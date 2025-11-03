c1cd86ac973c286882b4c35969154a3c5d6837ed
yanmo <67538999+yanmoyy@users.noreply.github.com>
2025-11-03 17:38:51 +0900

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine(
+            "What is your name?"
+        );
+        var name = Console.ReadLine();
+        var currentDate = DateTime.Now;
+        Console.WriteLine(
+            $"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!"
+        );
+        Console.Write(
+            $"{Environment.NewLine}Press Enter to exit..."
+        );
+        Console.Read();
+    }
+}
